47fb471c69f12025c0f0b61d162837de5d74e2d2
djconly85 <dconly@sacog.org>
2024-10-09 10:25:52 -0700

    more function to update_layer_sources.py for aprx layer updating

--- APRX/update_layer_sources.py
+++ APRX/update_layer_sources.py
@@ -12,11 +12,14 @@ Python Version: 3.x
 """
 
 import os
+from pathlib import Path
+from copy import deepcopy
 
 import arcpy
 
 
-def find_replace_all(aprx_path, old_src_path, new_src_path):
+
+def update_all_aprx_sources(aprx_path, old_src_path, new_src_path):
     # for all layers in the APRX project, if source layer = old_src_path,
     # replace it with new_src_path
     
@@ -29,6 +32,26 @@ def find_replace_all(aprx_path, old_src_path, new_src_path):
     aprx.save()
 
     print(f"Finished, replaced all instances of {old_src_path} with {new_src_path}")
+
+
+def update_map_layr_sources(aprx_path, map_name, old_src_path, new_src_path):
+    # to update all layers within specific map of APRX project
+    # 10/9/2024 - only tested on feature classes; may need modifying to work with shapefiles or other data source types.
+    old_dataset = Path(old_src_path)
+    new_dataset = Path(new_src_path)
+
+    aprx = arcpy.mp.ArcGISProject(aprx_path)
+    map_obj = aprx.listMaps(f"{map_name}*")[0]
+    
+    for lyr in map_obj.listLayers():
+        if lyr.connectionProperties:
+            oldprops = lyr.connectionProperties
+            newprops = deepcopy(oldprops)
+            newprops['connection_info']['database'] = str(new_dataset.parent)
+            newprops['dataset'] = new_dataset.name
+            lyr.updateConnectionProperties(oldprops, newprops)
+        
+    print([l.dataSource for l in map_obj.listLayers()])
 
 
 if __name__ == '__main__':
